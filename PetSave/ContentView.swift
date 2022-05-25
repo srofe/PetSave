e833faa491d9c452d2138f9083ed153109af2407
Simon Rofe <simon.rofe@ieee.org>
2022-05-25 13:37:48 +1000

    Initial Commit

---
+++ PetSave/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PetSave
+//
+//  Created by Simon Rofe on 25/5/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PetSave/PetSaveApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetSaveApp.swift
+//  PetSave
+//
+//  Created by Simon Rofe on 25/5/2022.
+//
+
+import SwiftUI
+
+@main
+struct PetSaveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

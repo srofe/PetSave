dc519e6d9768410f5f7f6fab2769370c91c9a977
Simon Rofe <simon.rofe@ieee.org>
2022-05-25 14:32:48 +1000

    Update unit test files to be same as starter project
    YouTrack Issue: ^PS-1

---
+++ PetSaveTests/Tests/Core/data/coreData/CoreDataTests.swift
@@ -0,0 +1,21 @@
+//
+//  CoreDataTests.swift
+//  PetSaveTests
+//
+//  Created by Simon Rofe on 25/5/2022.
+//
+
+import XCTest
+@testable import PetSave
+import CoreData
+
+class CoreDataTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+    }
+
+    override func tearDownWithError() throws {
+        try super.tearDownWithError()
+    }
+}

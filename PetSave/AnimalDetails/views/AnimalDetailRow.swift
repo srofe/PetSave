89c78b19fd136835401ef30606f38355dad27639
Simon Rofe <simon.rofe@ieee.org>
2022-05-27 05:55:06 +1000

    Add animal detail row view to starter project
    YouTrack Issue: ^PS-1

---
+++ PetSave/AnimalDetails/views/AnimalDetailRow.swift
@@ -0,0 +1,21 @@
+//
+//  AnimalDetailRow.swift
+//  PetSave
+//
+//  Created by Simon Rofe on 27/5/2022.
+//
+
+import SwiftUI
+
+struct AnimalDetailRow: View {
+    var body: some View {
+        Text("TODO: Animal Details Row")
+    }
+}
+
+struct AnimalDetailRow_Previews: PreviewProvider {
+    static var previews: some View {
+        AnimalDetailRow()
+            .previewLayout(.sizeThatFits)
+    }
+}

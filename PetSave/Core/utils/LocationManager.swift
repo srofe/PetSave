53216fd96463f6f4da2a18e1c1dfece132c6520b
Simon Rofe <simon.rofe@ieee.org>
2022-05-26 12:13:38 +1000

    Add location manager view model for starter project
    YouTrack Issue: ^PS-1

---
+++ PetSave/Core/utils/LocationManager.swift
@@ -0,0 +1,54 @@
+//
+// Created by Simon Rofe on 26/5/22.
+//
+
+import CoreLocation
+import SwiftUI
+
+final class LocationManager: NSObject, ObservableObject {
+    @Published var authorizationStatus: CLAuthorizationStatus
+    @Published var lastSeenLocation: CLLocation?
+    private let cllLocationManager: CLLocationManager
+
+    init(authorizationStatus: CLAuthorizationStatus = .notDetermined) {
+        self.authorizationStatus = authorizationStatus
+        self.cllLocationManager = CLLocationManager()
+        super.init()
+        cllLocationManager.delegate = self
+        self.authorizationStatus = cllLocationManager.authorizationStatus
+        cllLocationManager.startUpdatingLocation()
+    }
+
+    func updateAuthorizationStatus() {
+        authorizationStatus = cllLocationManager.authorizationStatus
+    }
+}
+
+extension LocationManager {
+    var locationIsDisabled: Bool {
+        authorizationStatus == .denied ||
+            authorizationStatus == .notDetermined ||
+            authorizationStatus == .restricted
+    }
+}
+
+extension LocationManager: CLLocationManagerDelegate {
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        updateAuthorizationStatus()
+    }
+
+    func locationManager(
+        _ manager: CLLocationManager,
+        didUpdateLocations locations: [CLLocation]
+    ) {
+        guard let location = locations.first else { return }
+        lastSeenLocation = location
+    }
+
+    func locationManager(
+        _ manager: CLLocationManager,
+        didFailWithError error: Error
+    ) {
+        print("Location retrieving failed due to: \(error.localizedDescription)")
+    }
+}

b71e356c609f81d0596080ad7eb7d4b269767eb1
Simon Rofe <simon.rofe@ieee.org>
2022-05-26 11:42:38 +1000

    Add size data models for starter project
    YouTrack Issue: ^PS-1

---
+++ PetSave/Core/domain/model/animal/Size.swift
@@ -0,0 +1,11 @@
+//
+// Created by Simon Rofe on 26/5/22.
+//
+
+enum Size: String, Codable {
+    case small = "Small"
+    case medium = "Medium"
+    case large = "Large"
+    case extraLarge = "Extra Large"
+    case unknown = "Unknown"
+}

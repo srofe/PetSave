2e4db6f80e74e02461db2581bb938ed8277f4aec
Simon Rofe <simon.rofe@ieee.org>
2022-05-26 11:31:52 +1000

    Add coat data model for starter project
    YouTrack Issue: ^PS-1

---
+++ PetSave/Core/domain/model/animal/Coat.swift
@@ -0,0 +1,13 @@
+//
+// Created by Simon Rofe on 26/5/22.
+//
+
+enum Coat: String, Codable {
+    case short = "Short"
+    case medium = "Medium"
+    case long = "Long"
+    case wire = "Wire"
+    case hairless = "Hairless"
+    case curly = "Curly"
+    case unknown = "Unknown"
+}

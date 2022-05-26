c4e4b52b186215aa44d07dbb7ad20fb0abd502d6
Simon Rofe <simon.rofe@ieee.org>
2022-05-26 11:27:35 +1000

    Add API colours data model for starter project
    YouTrack Issue: ^PS-1

---
+++ PetSave/Core/domain/model/animal/APIColors.swift
@@ -0,0 +1,10 @@
+//
+// Created by Simon Rofe on 26/5/22.
+//
+
+struct APIColors: Codable {
+    var id: Int?
+    var primary: String?
+    var secondary: String? = ""
+    var tertiary: String? = ""
+}

a2a7807533db059a3f91d944de0acb22fef008b4
Simon Rofe <simon.rofe@ieee.org>
2022-05-27 05:58:36 +1000

    Add animal details view to starter project
    YouTrack Issue: ^PS-1

---
+++ PetSave/AnimalDetails/views/AnimalDetailsView.swift
@@ -0,0 +1,30 @@
+//
+//  AnimalDetailsView.swift
+//  PetSave
+//
+//  Created by Simon Rofe on 27/5/2022.
+//
+
+import SwiftUI
+
+struct AnimalDetailsView: View {
+    var body: some View {
+        Text("TODO: Animlal Details")
+    }
+}
+
+struct AnimalDetailsView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            AnimalDetailsView()
+        }
+        .previewLayout(.sizeThatFits)
+        .previewDisplayName("iPhone SE (2nd generation)")
+
+        NavigationView {
+            AnimalDetailsView()
+        }
+        .previewDevice("iPhone 12 Pro")
+        .previewDisplayName("iPhone 12 Pro")
+    }
+}

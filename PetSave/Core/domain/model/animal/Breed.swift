a98519a76c4b3b7672dac4601787d9d772556c90
Simon Rofe <simon.rofe@ieee.org>
2022-05-26 11:29:30 +1000

    Add breed data model for starter project
    YouTrack Issue: ^PS-1

---
+++ PetSave/Core/domain/model/animal/Breed.swift
@@ -0,0 +1,11 @@
+//
+// Created by Simon Rofe on 26/5/22.
+//
+
+struct Breed: Codable {
+    var id: Int?
+    var primary: String?
+    var secondary: String?
+    var mixed: Bool?
+    var unknown: Bool?
+}

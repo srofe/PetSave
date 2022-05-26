0b7a21f0c0f1dfb0ed2fac765b448b6bb3723217
Simon Rofe <simon.rofe@ieee.org>
2022-05-26 19:33:40 +1000

    Add animal detail card to starter project
    YouTrack Issue: ^PS-1

---
+++ PetSave/AnimalDetails/views/AnimalDetailCard.swift
@@ -0,0 +1,45 @@
+//
+//  AnimalDetailCard.swift
+//  PetSave
+//
+//  Created by Simon Rofe on 26/5/2022.
+//
+
+import SwiftUI
+
+struct AnimalDetailCard: View {
+    let title: String
+    let value: String
+    let color: Color
+
+    var body: some View {
+        VStack {
+            Text(title)
+                .font(.subheadline)
+            Text(value)
+                .font(.headline)
+        }
+        .padding(.vertical)
+        .frame(width: 96)
+        .background(color.opacity(0.2))
+        .foregroundColor(color)
+        .cornerRadius(8)
+    }
+}
+
+struct AnimalDetailCard_Previews: PreviewProvider {
+    static var previews: some View {
+        HStack {
+            AnimalDetailCard(
+                title: "Age",
+                value: "Adult",
+                color: .green
+            )
+            AnimalDetailCard(
+                title: "Age",
+                value: "Baby",
+                color: .blue
+            )
+        }
+    }
+}

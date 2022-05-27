d6e535a677842757c574a7bae47c05b3848a8041
Simon Rofe <simon.rofe@ieee.org>
2022-05-27 19:28:22 +1000

    Add suggestions grid view to starter project
    YouTrack Issue: ^PS-1

---
+++ PetSave/Search/views/SuggestionsGrid.swift
@@ -0,0 +1,44 @@
+//
+//  SuggestionsGrid.swift
+//  PetSave
+//
+//  Created by Simon Rofe on 27/5/2022.
+//
+
+import SwiftUI
+
+struct SuggestionsGrid: View {
+    @Environment(\.isSearching) var isSearching: Bool
+    let suggestions: [AnimalSearchType]
+    var action: (AnimalSearchType) -> Void
+
+    private let columns = [
+        GridItem(.flexible()),
+        GridItem(.flexible())
+    ]
+
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("Browse by Type")
+                .font(.title2.bold())
+            LazyVGrid(columns: columns) {
+                ForEach(AnimalSearchType.suggestions, id: \.self) { suggestion in
+                    Button {
+                        action(suggestion)
+                    } label: {
+                        AnimalTypeSuggestionView(suggestion: suggestion)
+                    }
+                    .buttonStyle(.plain)
+                }
+            }
+        }
+        .padding(.horizontal)
+        .opacity(isSearching ? 0 : 1)
+    }
+}
+
+struct SuggestionsGrid_Previews: PreviewProvider {
+    static var previews: some View {
+        SuggestionsGrid(suggestions: AnimalSearchType.suggestions) { _ in }
+    }
+}

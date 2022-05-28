861ac85dddc1ec73f62558442edee5723a8f6644
Simon Rofe <simon.rofe@ieee.org>
2022-05-28 16:44:29 +1000

    Add animal data model
    YouTrack Issue: ^PS-2

---
+++ PetSave/Core/domain/model/animal/Animal.swift
@@ -0,0 +1,34 @@
+//
+//  Animal.swift
+//  PetSave
+//
+//  Created by Simon Rofe on 28/5/2022.
+//
+
+import Foundation
+
+struct Animal: Codable {
+    var id: Int?
+    let organizationId: String?
+    let url: URL?
+    let type: String
+    let species: String?
+    var breeds: Breed
+    var colors: APIColors
+    let age: Age
+    let gender: Gender
+    let size: Size
+    let coat: Coat?
+    let name: String
+    let description: String?
+    let photos: [PhotoSizes]
+    let videos: [VideoLink]
+    let status: AdoptionStatus
+    var attributes: AnimalAttributes
+    var environment: AnimalEnvironment?
+    let tags: [String]
+    var contact: Contact
+    let publishedAt: String?
+    let distance: Double?
+    var ranking: Int? = 0
+}

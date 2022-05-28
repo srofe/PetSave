82a48fbb786263c43a5dd9ab42ad6fbe14ca0b74
Simon Rofe <simon.rofe@ieee.org>
2022-05-28 16:36:10 +1000

    Add API key and secret
    YouTrack Issue: ^PS-2

--- PetSave/Core/data/api/APIConstants.swift
+++ PetSave/Core/data/api/APIConstants.swift
@@ -5,6 +5,6 @@
 enum APIConstants {
     static let host = "api.petfinder.com"
     static let grantType = "client_credentials"
-    static let clientId = "YourKeyHere"
-    static let clientSecret = "YourSecretHere"
+    static let clientId = "L4x2yM3BrxnyHezj2JUt1Me0jWMVJbBOWPuMP7pLCH8xunjfO5"
+    static let clientSecret = "7AUcYqu5PhBUCF6VAEV3MUM9oSmaiRI3S4QPAmP7"
 }

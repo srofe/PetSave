7255b6a9943cd03dec2da4a81070b321f420f2df
Simon Rofe <simon.rofe@ieee.org>
2022-05-29 19:41:17 +1000

    Add data parser data model for starting
    YouTrack Issue: ^PS-3

---
+++ PetSave/Core/data/api/parser/DataParser.swift
@@ -0,0 +1,22 @@
+//
+// Created by Simon Rofe on 29/5/2022.
+//
+
+import Foundation
+
+protocol DataParserProtocol {
+    func parse<T: Decodable>(data: Data) throws -> T
+}
+
+class DataParser: DataParserProtocol {
+    private var jsonDecoder: JSONDecoder
+
+    init(jsonDecoder: JSONDecoder = JSONDecoder()) {
+        self.jsonDecoder = jsonDecoder
+        self.jsonDecoder.keyDecodingStrategy = .convertFromSnakeCase
+    }
+
+    func parse<T: Decodable>(data: Data) throws -> T {
+        return try jsonDecoder.decode(T.self, from: data)
+    }
+}

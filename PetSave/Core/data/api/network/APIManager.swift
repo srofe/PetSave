8837f9c8268066ec2663429ea2ec5453277a8020
Simon Rofe <simon.rofe@ieee.org>
2022-05-30 13:59:01 +1000

    Add API manager for managing URL session
    This provides an abstraction layer above URLSession and between a
    request manager.
    
    YouTrack Issue: ^PS-3

---
+++ PetSave/Core/data/api/network/APIManager.swift
@@ -0,0 +1,29 @@
+//
+//  APIManager.swift
+//  PetSave
+//
+//  Created by Simon Rofe on 30/5/22.
+//
+
+import Foundation
+
+protocol APIManagerProtocol {
+    func perform(_ request: RequestProtocol, authToken: String) async throws -> Data
+}
+
+class APIManager: APIManagerProtocol {
+    private let urlSession: URLSession
+
+    init(urlSession: URLSession = URLSession.shared) {
+        self.urlSession = urlSession
+    }
+
+    func perform(_ request: RequestProtocol, authToken: String = "") async throws -> Data {
+        let (data, response) = try await urlSession.data(for: request.createURLRequest(authToken: authToken))
+        guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
+            throw NetworkError.invalidServerResponse
+        }
+
+        return data
+    }
+}

3b29edac0d6f29b806a1aca3a335b5c3b0ac41dd
Simon Rofe <simon.rofe@ieee.org>
2022-05-30 17:23:36 +1000

    Add animals reqest
    YouTrack Issue: ^PS-3

---
+++ PetSave/Core/data/api/request/animals/AnimalsRequest.swift
@@ -0,0 +1,46 @@
+//
+//  AnimalsRequest.swift
+//  PetSave
+//
+//  Created by Simon Rofe on 30/5/2022.
+//
+
+import Foundation
+
+enum AnimalsRequest: RequestProtocol {
+    case getAnimalsWith(page: Int, latitude: Double?, longitude: Double?)
+    case getAnimalsBy(name: String, age: String?, type: String?)
+
+    var path: String {
+        "/v2/animals"
+    }
+    var urlParams: [String : String?] {
+        switch self {
+            case let .getAnimalsWith(page, latitude, longitude):
+                var params = ["page":String(page)]
+                if let latitude = latitude {
+                    params["latitude"] = String(latitude)
+                }
+                if let longitude = longitude {
+                    params["longitude"] = String(longitude)
+                }
+                params["sort"] = "random"
+                return params
+            case let .getAnimalsBy(name, age, type):
+                var params: [String:String] = [:]
+                if !name.isEmpty {
+                    params["name"] = name
+                }
+                if let age = age {
+                    params["age"] = age
+                }
+                if let type = type {
+                    params["type"] = type
+                }
+                return params
+        }
+    }
+    var requestType: RequestType {
+        .GET
+    }
+}

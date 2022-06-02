0c2129b4840adaa764488ab6f3b7e1af182ab2d5
Simon Rofe <simon.rofe@ieee.org>
2022-06-03 09:59:40 +1000

    Implement address entity for starter project
    YouTrack Issue: ^PS-4

--- PetSave/Core/data/coreData/entities/AddressEntity.swift
+++ PetSave/Core/data/coreData/entities/AddressEntity.swift
@@ -9,4 +9,28 @@ import CoreData
 
 @objc(AddressEntity)
 public class AddressEntity: NSManagedObject {
+    @NSManaged public var id: Int64
+    @NSManaged public var address1: String?
+    @NSManaged public var address2: String?
+    @NSManaged public var city: String
+    @NSManaged public var postcode: String
+    @NSManaged public var state: String
+    @NSManaged public var country: String
+    @NSManaged public var contact: NSSet?
 }
+
+extension AddressEntity {
+    @objc(addContactObject:)
+    @NSManaged public func addToContact(_ value: ContactEntity)
+
+    @objc(removeContactObject:)
+    @NSManaged public func removeFromContact(_ value: ContactEntity)
+
+    @objc(addContact:)
+    @NSManaged public func addToContact(_ values: NSSet)
+
+    @objc(removeContact:)
+    @NSManaged public func removeFromContact(_ values: NSSet)
+}
+
+extension AddressEntity: Identifiable {}

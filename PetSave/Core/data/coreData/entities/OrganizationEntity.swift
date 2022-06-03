84c751159c94bbf6861c8dfaa8efb6b0537d0b42
Simon Rofe <simon.rofe@ieee.org>
2022-06-03 17:10:14 +1000

    Implement organization entity for starter project
    YouTrack Issue: ^PS-4

--- PetSave/Core/data/coreData/entities/OrganizationEntity.swift
+++ PetSave/Core/data/coreData/entities/OrganizationEntity.swift
@@ -9,4 +9,13 @@ import CoreData
 
 @objc(OrganizationEntity)
 public class OrganizationEntity: NSManagedObject {
+    @NSManaged public var distance: Double
+    @NSManaged public var id: Int64
+    @NSManaged public var contact: ContactEntity?
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<OrganizationEntity> {
+        NSFetchRequest<OrganizationEntity>(entityName: "OrganizationEntity")
+    }
 }
+
+extension OrganizationEntity: Identifiable {}

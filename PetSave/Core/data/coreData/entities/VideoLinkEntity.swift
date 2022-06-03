28247d7e064e727b02e28535c63f5cfa56065e64
Simon Rofe <simon.rofe@ieee.org>
2022-06-03 17:25:49 +1000

    Implement video link entity for starter project
    YouTrack Issue: ^PS-4

--- PetSave/Core/data/coreData/entities/VideoLinkEntity.swift
+++ PetSave/Core/data/coreData/entities/VideoLinkEntity.swift
@@ -9,4 +9,13 @@ import CoreData
 
 @objc(VideoLinkEntity)
 public class VideoLinkEntity: NSManagedObject {
+    @NSManaged public var embedded: URL?
+    @NSManaged public var id: Int64
+    @NSManaged public var animal: AnimalEntity?
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<VideoLinkEntity> {
+        NSFetchRequest<VideoLinkEntity>(entityName: "VideoLinkEntity")
+    }
 }
+
+extension VideoLinkEntity: Identifiable {}

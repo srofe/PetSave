b1b8fca10655812692e2cd28d12d763f5743a2c4
Simon Rofe <simon.rofe@ieee.org>
2022-06-03 17:04:59 +1000

    Implement contact entity for starter project
    YouTrack Issue: ^PS-4

--- PetSave/Core/data/coreData/entities/ContactEntity.swift
+++ PetSave/Core/data/coreData/entities/ContactEntity.swift
@@ -9,4 +9,30 @@ import CoreData
 
 @objc(ContactEntity)
 public class ContactEntity: NSManagedObject {
+    @NSManaged public var email: String?
+    @NSManaged public var id: Int64
+    @NSManaged public var phone: String?
+    @NSManaged public var address: AddressEntity?
+    @NSManaged public var animal: AnimalEntity?
+    @NSManaged public var organization: NSSet?
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<ContactEntity> {
+        NSFetchRequest<ContactEntity>(entityName: "ContactEntity")
+    }
+}
+
+extension ContactEntity: Identifiable {}
+
+extension ContactEntity {
+    @objc(addOrganizationObject:)
+    @NSManaged public func addToOrganization(_ value: OrganizationEntity)
+
+    @objc(removeOrganizationObject:)
+    @NSManaged public func removeFromOrganization(_ value: OrganizationEntity)
+
+    @objc(addOrganization:)
+    @NSManaged public func addToOrganization(_ values: NSSet)
+
+    @objc(removeOrganization:)
+    @NSManaged public func removeFromOrganization(_ values: NSSet)
 }

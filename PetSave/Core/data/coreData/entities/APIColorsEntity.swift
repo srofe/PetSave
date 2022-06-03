cbaf622dccd2d63c71eea7efd2d9349ad6c52d6d
Simon Rofe <simon.rofe@ieee.org>
2022-06-03 16:56:13 +1000

    Implement API colors entity for starter project
    YouTrack Issue: ^PS-4

--- PetSave/Core/data/coreData/entities/APIColorsEntity.swift
+++ PetSave/Core/data/coreData/entities/APIColorsEntity.swift
@@ -9,4 +9,15 @@ import CoreData
 
 @objc(APIColorsEntity)
 public class APIColorsEntity: NSManagedObject {
+    @NSManaged public var id: Int64
+    @NSManaged public var primary: String?
+    @NSManaged public var secondary: String?
+    @NSManaged public var tertiary: String?
+    @NSManaged public var animal: AnimalEntity?
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<APIColorsEntity> {
+        NSFetchRequest<APIColorsEntity>(entityName: "APIColorsEntity")
+    }
 }
+
+extension APIColorsEntity: Identifiable {}

8a7c3e82178239e401fe1ffdc049b544e67eeaa9
Simon Rofe <simon.rofe@ieee.org>
2022-06-06 12:25:45 +1000

    Add core data persistable protocol
    This is used to assist with converting between data model types (which
    are structs) and Core Data types (which are classes).
    
    YouTrack Issue: ^PS-4

--- PetSave/Core/data/coreData/extensions/CoreDataPersistable.swift
+++ PetSave/Core/data/coreData/extensions/CoreDataPersistable.swift
@@ -10,3 +10,14 @@ import CoreData
 protocol UUIDIdentifiable: Identifiable {
     var id: Int? { get set }
 }
+
+protocol CoreDataPersistable: UUIDIdentifiable {
+    associatedtype ManagedType
+
+    var keyMap: [PartialKeyPath<Self>: String] { get }
+
+    init()
+    init(managedObject: ManagedType?)
+    mutating func toManagedObject(context: NSManagedObjectContext) -> ManagedType
+    func save(context: NSManagedObjectContext) throws
+}

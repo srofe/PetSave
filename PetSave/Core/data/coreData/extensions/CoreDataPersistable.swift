c82d725766f7be8500c69b41aa2d47ee11aae5ba
Simon Rofe <simon.rofe@ieee.org>
2022-06-06 12:42:25 +1000

    Add default implementation of init() method
    YouTrack Issue: ^PS-4

--- PetSave/Core/data/coreData/extensions/CoreDataPersistable.swift
+++ PetSave/Core/data/coreData/extensions/CoreDataPersistable.swift
@@ -21,3 +21,31 @@ protocol CoreDataPersistable: UUIDIdentifiable {
     mutating func toManagedObject(context: NSManagedObjectContext) -> ManagedType
     func save(context: NSManagedObjectContext) throws
 }
+
+extension CoreDataPersistable where ManagedType: NSManagedObject {
+    init(managedObject: ManagedType?) {
+        self.init()
+        guard let managedObject = managedObject else { return }
+        for attribute in managedObject.entity.attributesByName {
+            if let keyP = keyMap.first(where: { $0.value == attribute.key })?.key {
+                let value = managedObject.value(forKey: attribute.key)
+                storeValue(value, toKeyPath: keyP)
+            }
+        }
+    }
+
+    private mutating func storeValue(_ value: Any?, toKeyPath partial: AnyKeyPath) {
+        switch partial {
+        case let keyPath as WritableKeyPath<Self, URL?>:
+            self[keyPath: keyPath] = value as? URL
+        case let keyPath as WritableKeyPath<Self, Int?>:
+            self[keyPath: keyPath] = value as? Int
+        case let keyPath as WritableKeyPath<Self, String?>:
+            self[keyPath: keyPath] = value as? String
+        case let keyPath as WritableKeyPath<Self, Bool?>:
+            self[keyPath: keyPath] = value as? Bool
+        default:
+            return
+        }
+    }
+}

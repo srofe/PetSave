e10555ce2505300434cf2ed6dc9a8f255e0c7dfa
Simon Rofe <simon.rofe@ieee.org>
2022-06-06 13:59:03 +1000

    Add CoreDataPersistable extension to Breed
    YouTrack Issue: ^PS-4

--- PetSave/Core/data/coreData/extensions/Breed+CoreData.swift
+++ PetSave/Core/data/coreData/extensions/Breed+CoreData.swift
@@ -5,4 +5,18 @@
 //  Created by Simon Rofe on 6/6/2022.
 //
 
-import Foundation
+import CoreData
+
+extension Breed: CoreDataPersistable {
+    var keyMap: [PartialKeyPath<Breed> : String] {
+        [
+            \.primary: "primary",
+             \.secondary: "secondary",
+             \.mixed: "mixed",
+             \.unknown: "unknown",
+             \.id: "id"
+        ]
+    }
+
+    typealias ManagedType = BreedEntity
+}

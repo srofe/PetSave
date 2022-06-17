a76046aa56f996650796eb680fdefbcb1fb47ad7
Simon Rofe <simon.rofe@ieee.org>
2022-06-18 07:45:54 +1000

    Verify empty response from animal fetcher
    YouTrack Issue: ^PS-5

--- PetSaveTests/Tests/AnimalsNearYou/AnimalsNearYouViewModelTestCase.swift
+++ PetSaveTests/Tests/AnimalsNearYou/AnimalsNearYouViewModelTestCase.swift
@@ -26,9 +26,26 @@ class AnimalsNearYouViewModelTestCase: XCTestCase {
         XCTAssertFalse(viewModel.isLoading, "The view model shouldn't be loading, but it is")
     }
 
-    func testUpdatePageFetchMoreAnaimals() async {
+    func testUpdatePageOnFetchMoreAnimals() async {
         XCTAssertEqual(viewModel.page, 1, "the view model's page property should be 1 before fetching, but it's \(viewModel.page)")
         await viewModel.fetchMoreAnimals()
         XCTAssertEqual(viewModel.page, 2, "the view model's page property should be 2 after fetching, but it's \(viewModel.page)")
+    }
+
+    func testFetchAnimalsEmptyResponse() async {
+        viewModel = AnimalsNearYouViewModel(
+            isLoading: true,
+            animalFetcher: EmptyResponseAnimalsFetcherMock(),
+            animalStore: AnimalStoreService(context: testContext)
+        )
+        await viewModel.fetchAnimals()
+        XCTAssertFalse(viewModel.hasMoreAnimals,"hasMoreAnimals should be false with an empty response, but it's true")
+        XCTAssertFalse(viewModel.isLoading,"the view model shouldn't be loading after receiving an empty response, but it is")
+    }
+}
+
+struct EmptyResponseAnimalsFetcherMock: AnimalsFetcher {
+    func fetchAnimals(page: Int) async -> [Animal] {
+        []
     }
 }

ae822d48cd63d3abf651782d41b135a582e5cdb8
Simon Rofe <simon.rofe@ieee.org>
2022-06-18 15:43:06 +1000

    Add search bar to search view
    YouTrack Issue: ^PS-6

--- PetSave/Search/views/SearchView.swift
+++ PetSave/Search/views/SearchView.swift
@@ -10,18 +10,18 @@ import SwiftUI
 struct SearchView: View {
     @FetchRequest(
         sortDescriptors: [
-            NSSortDescriptor(
-                keyPath: \AnimalEntity.timestamp, ascending: true
-            )
+            NSSortDescriptor(keyPath: \AnimalEntity.timestamp, ascending: true)
         ],
         animation: .default
     )
     private var animals: FetchedResults<AnimalEntity>
+    @State var searchText = ""
 
     var body: some View {
         NavigationView {
             AnimalListView(animals: animals)
                 .navigationTitle("Find your future pet")
+                .searchable(text: $searchText, placement: .navigationBarDrawer(displayMode: .always))
         }
         .navigationViewStyle(StackNavigationViewStyle())
     }

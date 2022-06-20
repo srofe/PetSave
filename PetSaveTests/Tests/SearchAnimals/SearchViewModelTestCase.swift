22d1c7265f6cc8214c72c2756f2b6ac518c53bab
Simon Rofe <simon.rofe@ieee.org>
2022-06-20 10:44:09 +1000

    Add test for default empty search properties
    YouTrack Issue: ^PS-6

---
+++ PetSaveTests/Tests/SearchAnimals/SearchViewModelTestCase.swift
@@ -0,0 +1,29 @@
+//
+//  SearchViewModelTestCase.swift
+//  PetSaveTests
+//
+//  Created by Simon Rofe on 20/6/2022.
+//
+
+import XCTest
+@testable import PetSave
+
+class SearchViewModelTestCase: XCTestCase {
+    let testContext = PersistenceController.preview.container.viewContext
+    var viewModel: SearchViewModel!
+
+    override func setUp() {
+        super.setUp()
+        viewModel = SearchViewModel(
+            animalSearcher: AnimalSearcherMock(),
+            animalStore: AnimalStoreService(context: testContext)
+        )
+    }
+
+    func testShouldFilterIsFalseForEmptyFilters() {
+        XCTAssertTrue(viewModel.searchText.isEmpty)
+        XCTAssertEqual(viewModel.ageSelection, .none)
+        XCTAssertEqual(viewModel.typeSelection, .none)
+        XCTAssertFalse(viewModel.shouldFilter)
+    }
+}

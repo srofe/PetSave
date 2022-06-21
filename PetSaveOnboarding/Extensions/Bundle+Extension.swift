d659ae5195b74e0d82635e27c75aa66e6fdd3d8f
Simon Rofe <simon.rofe@ieee.org>
2022-06-21 11:46:36 +1000

    Add extensions for assets
    YouTrack Issue: ^PS-7

---
+++ PetSaveOnboarding/Extensions/Bundle+Extension.swift
@@ -0,0 +1,12 @@
+//
+//  Bundle+Extension.swift
+//  PetSaveOnboarding
+//
+//  Created by Simon Rofe on 21/6/2022.
+//
+
+extension Bundle {
+    public static var module: Bundle? {
+        Bundle(identifier: "com.poddlybonk.PetSaveOnboarding")
+    }
+}

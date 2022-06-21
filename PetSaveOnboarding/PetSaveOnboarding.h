5eaba357d5bda7e7eec4ae9128477215e88b4df3
Simon Rofe <simon.rofe@ieee.org>
2022-06-21 11:17:50 +1000

    Add empty framework to the project
    YouTrack Issue: ^PS-7

---
+++ PetSaveOnboarding/PetSaveOnboarding.h
@@ -0,0 +1,18 @@
+//
+//  PetSaveOnboarding.h
+//  PetSaveOnboarding
+//
+//  Created by Simon Rofe on 21/6/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PetSaveOnboarding.
+FOUNDATION_EXPORT double PetSaveOnboardingVersionNumber;
+
+//! Project version string for PetSaveOnboarding.
+FOUNDATION_EXPORT const unsigned char PetSaveOnboardingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PetSaveOnboarding/PublicHeader.h>
+
+

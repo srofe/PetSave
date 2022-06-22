7662357fe8a54d272f780b25bba8cb98fb33b625
Simon Rofe <simon.rofe@ieee.org>
2022-06-22 17:49:18 +1000

    Update to be same a starter project for chapter
    YouTrack Issue: ^PS-9

--- PetSave/AnimalDetails/views/AnimalHeaderView.swift
+++ PetSave/AnimalDetails/views/AnimalHeaderView.swift
@@ -8,8 +8,24 @@
 import SwiftUI
 
 struct AnimalHeaderView: View {
+    let animal: AnimalEntity
+    @Binding var zoomed: Bool
+    @Binding var favorited: Bool
+    let geometry: GeometryProxy
+
     var body: some View {
-        Text("TODO: Animal Header View")
+        if zoomed {
+            LazyVStack {
+                AnimalImage(animalPicture: animal.picture, zoomed: $zoomed, geometry: geometry)
+                HeaderTitle(animal: animal, zoomed: $zoomed, geometry: geometry)
+            }
+        } else {
+            HStack {
+                AnimalImage(animalPicture: animal.picture, zoomed: $zoomed, geometry: geometry)
+                HeaderTitle(animal: animal,zoomed: $zoomed, geometry: geometry)
+                Spacer(minLength: 25)
+            }
+        }
     }
 }
 
@@ -42,9 +58,9 @@ struct AnimalImage: View {
             width: zoomed ? geometry.frame(in: .local).width : 100,
             height: zoomed ? geometry.frame(in: .global).midX : 100
         )
-        .position(
-            x: zoomed ? geometry.frame(in: .local).midX : 50,
-            y: zoomed ? geometry.frame(in: .global).midX : 50
+        .offset(
+            x: zoomed ? 0 : 0,
+            y: zoomed ? geometry.frame(in: .local).midY / 3 : 0
         )
         .scaleEffect((zoomed ? 5 : 3) / 3)
         .shadow(radius: zoomed ? 10 : 1)
@@ -53,19 +69,73 @@ struct AnimalImage: View {
 }
 
 struct HeaderTitle: View {
+    @Binding var zoomed: Bool
+    var geometry: GeometryProxy
+    let animalName: String?
+    let animalType: String?
+    let animal: AnimalEntity
+
+    init(animal: AnimalEntity, zoomed: Binding<Bool>, geometry: GeometryProxy) {
+        self.animal = animal
+        self.animalType = animal.type
+        self.animalName = animal.name
+        self._zoomed = zoomed
+        self.geometry = geometry
+    }
+
     var body: some View {
-        Text("TODO: Header Title")
+        VStack(alignment: .leading) {
+            Text(animalName ?? "Default Name")
+                .font(.largeTitle)
+                .frame(maxWidth: .infinity, alignment: zoomed ? .center : .leading)
+            Text("\(animal.breed) \(animalType ?? "")")
+                .font(.title3)
+                .frame(maxWidth: .infinity, alignment: zoomed ? .center : .leading)
+        }
+        .offset(
+            x: zoomed ? 0 : 0,
+            y: zoomed ? geometry.frame(in: .local).midY : 0
+        )
+        .animation(.spring(), value: zoomed)
     }
 }
 
 struct HeaderTitle_Previews: PreviewProvider {
     static var previews: some View {
-        HeaderTitle()
+        if let animal = CoreDataHelper.getTestAnimalEntity() {
+            Group {
+                GeometryReader { geometry in
+                    HeaderTitle(animal: animal, zoomed: .constant(true), geometry: geometry)
+                }
+                .frame(width: 200, height: 150)
+                GeometryReader { geometry in
+                    HeaderTitle(animal: animal, zoomed: .constant(false), geometry: geometry)
+                }
+                .frame(width: 200, height: 100)
+            }
+            .previewLayout(.sizeThatFits)
+        }
     }
 }
 
 struct AnimalHeaderView_Previews: PreviewProvider {
     static var previews: some View {
-        AnimalHeaderView()
+        if let animal = CoreDataHelper.getTestAnimalEntity() {
+            Group {
+                GeometryReader { geometry in
+                    AnimalHeaderView(animal: animal, zoomed: .constant(true), favorited: .constant(false), geometry: geometry)
+                }
+                .frame(width: 500, height: 700)
+                GeometryReader { geometry in
+                    AnimalHeaderView(animal: animal, zoomed: .constant(false), favorited: .constant(true), geometry: geometry)
+                }
+                .frame(width: 500, height: 100)
+                GeometryReader { geometry in
+                    AnimalHeaderView(animal: animal, zoomed: .constant(false), favorited: .constant(false), geometry: geometry)
+                }
+                .frame(width: 500, height: 100)
+            }
+            .previewLayout(.sizeThatFits)
+        }
     }
 }

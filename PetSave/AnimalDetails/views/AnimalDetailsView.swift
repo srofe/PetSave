93327860bee9272f2a16d9bedd2e03fa766450df
Simon Rofe <simon.rofe@ieee.org>
2022-06-22 16:56:23 +1000

    Update to be same a starter project for chapter
    YouTrack Issue: ^PS-9

--- PetSave/AnimalDetails/views/AnimalDetailsView.swift
+++ PetSave/AnimalDetails/views/AnimalDetailsView.swift
@@ -7,39 +7,70 @@
 
 import SwiftUI
 
-struct AnimalDetailsRouter: NavigationRouter {
-    typealias Data = AnimalEntity
-
-    func navigate<T: View>(data: AnimalEntity, navigationState: NavigationState, view: (() -> T)?) -> AnyView {
-        AnyView(NavigationLink(
-            destination: AnimalDetailsView(name: data.name ?? "")
-                .environmentObject(navigationState)
-        ) {
-            view?()
-        }
-        )
-    }
-}
-
 struct AnimalDetailsView: View {
-    var name: String
-    @EnvironmentObject var navigationState: NavigationState
+    @State var zoomed = false
+    @State var favorited = false
+    let animal: AnimalEntity
+    var animalDescription: String? {
+        animal.desc
+    }
+    var animalName: String? {
+        animal.name
+    }
 
     var body: some View {
-        Text(name)
-        Button(navigationState.isNavigatingDisabled ? "Enable Navigation" : "Disable Navigation") {
-            navigationState.isNavigatingDisabled.toggle()
+        GeometryReader { geometry in
+            ScrollView {
+                ZStack(alignment: .leading) {
+                    LazyVStack(alignment: .leading) {
+                        AnimalHeaderView(animal: animal, zoomed: $zoomed, favorited: $favorited, geometry: geometry)
+                            .onTapGesture { zoomed.toggle() }
+                        Divider()
+                            .blur(radius: zoomed ? 20 : 0)
+                        AnimalDetailRow(animal: animal)
+                            .blur(radius: zoomed ? 20 : 0)
+                        Divider()
+                            .blur(radius: zoomed ? 20 : 0)
+                        VStack(alignment: .leading, spacing: 24) {
+                            if let description = animalDescription {
+                                VStack(alignment: .leading) {
+                                    Text("Details")
+                                        .font(.headline)
+                                    Text(description)
+                                }
+                            }
+                            AnimalContactsView(animal: animal)
+                            Divider()
+                                .blur(radius: zoomed ? 20 : 0)
+                            AnimalLocationView(animal: animal)
+                        }
+                        .blur(radius: zoomed ? 20 : 0)
+                        .padding(.horizontal)
+                        .padding(.bottom)
+                    }
+                    .animation(.spring(), value: zoomed)
+                }
+            }
         }
+        .navigationTitle(animalName ?? "")
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
 struct AnimalDetailsView_Previews: PreviewProvider {
     static var previews: some View {
-        NavigationView {
-            AnimalDetailsView(name: "Snow")
-                .environmentObject(NavigationState())
+        if let animal = CoreDataHelper.getTestAnimalEntity() {
+            NavigationView {
+                AnimalDetailsView(animal: animal)
+                    .previewLayout(.sizeThatFits)
+            }
+            .previewLayout(.sizeThatFits)
+            .previewDisplayName("iPhone SE (2nd generation)")
+            NavigationView {
+                AnimalDetailsView(animal: animal)
+            }
+            .previewDevice("iPhone 12 Pro")
+            .previewDisplayName("iPhone 12 Pro")
         }
-        .previewDevice("iPhone 12 Pro")
-        .previewDisplayName("iPhone 12 Pro")
     }
 }

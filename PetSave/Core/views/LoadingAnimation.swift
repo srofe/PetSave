35becdb8f8f206d217f93fa9a503b91cad89dacd
Simon Rofe <simon.rofe@ieee.org>
2022-06-23 10:08:22 +1000

    Add animation view for loading more animals
    YouTrack Issue: ^PS-9

---
+++ PetSave/Core/views/LoadingAnimation.swift
@@ -0,0 +1,56 @@
+//
+//  LoadingAnimation.swift
+//  PetSave
+//
+//  Created by Simon Rofe on 23/6/2022.
+//
+
+import SwiftUI
+
+struct LoadingAnimation: UIViewRepresentable {
+    let animatedFrames: UIImage
+    let image: UIImageView
+    let squareDimension: CGFloat = 125
+
+    init() {
+        var images: [UIImage] = []
+
+        for i in 1 ... 127 {
+            guard let image = UIImage(named: "dog_\(String(format: "%03d", i))") else { continue }
+            images.append(image)
+        }
+        animatedFrames = UIImage.animatedImage(with: images, duration: 4) ?? UIImage()
+        image = UIImageView(frame: CGRect(x: 0, y: 0, width: squareDimension, height: squareDimension))
+    }
+
+    func makeUIView(context: Context) -> UIView {
+        let view = UIView(frame: CGRect(x: 0, y: 0, width: squareDimension, height: squareDimension))
+        image.clipsToBounds = true
+        image.autoresizesSubviews = true
+        image.contentMode = .scaleAspectFit
+        image.image = animatedFrames
+        image.center = CGPoint(x: view.frame.width / 2, y: view.frame.height / 2)
+        view.backgroundColor = .red
+        view.addSubview(image)
+        return view
+    }
+
+    func updateUIView(_ uiView: UIViewType, context: Context) {
+    }
+}
+
+struct LoadingAnimationView: View {
+    var body: some View {
+        VStack {
+            LoadingAnimation()
+        }
+    }
+}
+
+struct LoadingAnimationView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoadingAnimationView()
+            .frame(width: 125, height: 125, alignment: .center)
+            .previewLayout(.sizeThatFits)
+    }
+}
